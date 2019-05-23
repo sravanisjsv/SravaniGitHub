efe3ee16cb33e4524b63379082e30edd4c63b300
psravani <psravani@CHNSIPDT0T731>
2019-05-23 16:52:41 +0530

    program to add 2 numbers

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -8,8 +8,20 @@ namespace GitExample
 {
     class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of two numbers is : {0} \n", Sum(n1, n2));
         }
     }
 }
